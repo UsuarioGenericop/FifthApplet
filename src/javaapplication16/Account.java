e2b721ba15eb9df32dd6f97140df45d5374e93a9
Andres <andydquintero@gmail.com>
2025-09-30 15:36:26 -0500

    BuiltInterfaceAndAccountClass

---
+++ src/javaapplication16/Account.java
@@ -0,0 +1,13 @@
+package javaapplication16;
+
+public class Account {
+    int numeroCuenta;
+    String  nombre;
+    double saldo;
+
+    Account(int numeroCuenta, String nombre, double saldo){
+        this.numeroCuenta =  numeroCuenta;
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
